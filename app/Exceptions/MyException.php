9fbf82c727303332ae0b8a15f2f8915beb63be44
yqx254 <ravix254@outlook.com>
2020-09-07 14:13:59 +0800

    Custom Exception

---
+++ app/Exceptions/MyException.php
@@ -0,0 +1,17 @@
+<?php
+
+
+namespace App\Exceptions;
+
+
+class MyException extends \Exception{
+    //此异常抛出时如何报告异常
+    public function report(){
+        \Log::error("1111");
+    }
+
+    //异常抛出时如何向浏览器端反馈
+    public function render($request){
+        return successJson([]);
+    }
+}
--- app/Http/Controllers/ExceptionController.php
+++ app/Http/Controllers/ExceptionController.php
@@ -4,19 +4,13 @@
 namespace App\Http\Controllers;
 
 
+use App\Exceptions\MyException;
 use Illuminate\Database\Eloquent\ModelNotFoundException;
 use Illuminate\Http\Request;
 
 class ExceptionController extends Controller {
     //异常试玩
     public function exceptionIndex(Request $request){
-        try{
-            $a = 1;
-            throw new ModelNotFoundException();
-            return 1;
-        }
-        catch (\Exception $e){
-            return 0;
-        }
+        throw new MyException();
     }
 }
