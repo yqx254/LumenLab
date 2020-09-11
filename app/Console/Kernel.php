acc70973adf4041662e9603d97e4b573b8b652f1
yqx254 <ravix254@outlook.com>
2020-09-11 17:28:51 +0800

    schedule management

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -2,6 +2,7 @@
 
 namespace App\Console;
 
+use App\Jobs\MailJob;
 use Illuminate\Console\Scheduling\Schedule;
 use Laravel\Lumen\Console\Kernel as ConsoleKernel;
 
@@ -22,8 +23,30 @@ class Kernel extends ConsoleKernel
      * @param  \Illuminate\Console\Scheduling\Schedule  $schedule
      * @return void
      */
-    protected function schedule(Schedule $schedule)
-    {
-        //
+    protected function schedule(Schedule $schedule){
+        //使用call提交任务并设定提交频率，有调用命令类和闭包两种形式
+//        $schedule->call(new Command)->daily();
+//        $schedule->call(function(){
+//           DB::truncate("table");
+//        })->daily();
+
+        //使用command，有直接填写命令和填写命令类+参数数组两种形式
+//        $schedule->command("command Param --force")->daily();
+//        $schedule->command(Command::class,["Param","--force"])->daily();
+
+        //提交队列任务（等于dispatch）
+//        $schedule->job(new MailJob)->everyMinute();
+        //执行命令
+        $schedule->exec("php artisan ")->everyMinute();
+        //常见钩子
+        $schedule->command("some command")
+            ->everySixHours()
+            ->withoutOverlapping()
+            ->sendOutputTo("/storage/logs/schedule.log")
+            ->emailOutputOnFailure("ravix254@outlook.com")->before(function(){
+
+            })->after(function(){
+
+            });
     }
 }
